a5ec3ab417245b3e3170f995a0adfc3bb355d990
Kittitat Rodchaidee <rodch100@mail.chapman.edu>
2014-01-13 14:25:35 -0500

    Chapter Two Excercise

---
+++ chapterTwo/calc.rb
@@ -0,0 +1,8 @@
+puts "Hours in a year : " 
+puts 365 * 24 
+puts "Minutes in a decade : "
+puts (365 * 10) * 24 * 60
+puts "Your age in seconds : "
+puts (365 * 21) * 24 * 60 * 60
+puts "Our dear author's age : "
+puts (1160000000 / 60) / 60 / 24 / 365
