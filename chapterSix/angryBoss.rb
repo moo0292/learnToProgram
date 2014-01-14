5726d80a7bcc70ad90c8a3c0c6bd222d47925012
Kittitat Rodchaidee <rodch100@mail.chapman.edu>
2014-01-13 21:35:40 -0500

    Chapter Six Excercies

---
+++ chapterSix/angryBoss.rb
@@ -0,0 +1,4 @@
+puts 'Angry Boss Program'
+puts 'The boss asks what do you want'
+input = gets.chomp
+puts 'WHADDAYA MEAN "' + input.upcase + '"!? YOU\'RE FIRED'
---
+++ chapterSix/tableOfContents.rb
@@ -0,0 +1,6 @@
+line_width = 50
+puts ('Table of Contents'.center(line_width))
+puts ''
+puts (('Chapter 1: Getting Started'.ljust(line_width)) + ('page 1').rjust(line_width/2))
+puts (('Chapter 2: Numbers'.ljust(line_width)) + ('page 2').rjust(line_width/2))
+puts (('Chapter 3: Letters'.ljust(line_width)) + ('page 3').rjust(line_width/2))
